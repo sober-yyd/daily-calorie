8ed567eebb7afe9475a584f011d2f311ce6801fe
sober-yyd <18991386299@163.com>
2026-07-08 21:46:07 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+﻿const CACHE_NAME = 'calorie-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/bmr.js',
+  './js/food-db.js',
+  './js/exercise-db.js',
+  './js/gemini.js',
+  './js/storage.js',
+  './manifest.json',
+  './icons/icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
